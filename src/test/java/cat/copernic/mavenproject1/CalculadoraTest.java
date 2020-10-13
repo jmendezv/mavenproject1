16954b73f63a9fca473bd67da13303f0f36904d3
Pep Mendez <jmendez1@xtec.cat>
2020-10-13 09:24:38 +0200

    Added travis.yml file and tests

---
+++ src/test/java/cat/copernic/mavenproject1/CalculadoraTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.copernic.mavenproject1;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author pep
+ */
+public class CalculadoraTest extends TestCase {
+    
+    public CalculadoraTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of suma method, of class Calculadora.
+     */
+    public void testSuma() {
+        assertEquals(5, Calculadora.suma(3, 2));
+    }
+    
+}
