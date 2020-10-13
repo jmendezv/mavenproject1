2f98d4a29626f6723f767e3ef22f3522ad10130f
Pep Mendez <jmendez1@xtec.cat>
2020-10-13 09:18:44 +0200

    Added Calculadora class

---
+++ src/main/java/cat/copernic/mavenproject1/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.copernic.mavenproject1;
+
+/**
+ *
+ * @author pep
+ */
+public class Calculadora {
+    
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+    
+}
